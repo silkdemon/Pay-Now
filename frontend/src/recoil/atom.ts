f8b0cb41796af85fd47e02b61641a09fc55d87bd
Maria Shevchuk <ShevchukMV@cloudx.group>
2024-10-16 12:54:40 +0300

    fix dockerfile add docker-compose

--- frontend/src/recoil/atom.ts
+++ frontend/src/recoil/atom.ts
@@ -1,32 +1,31 @@
-import {atom} from 'recoil';
-
+import { atom } from "recoil";
 
 export const logined = atom({
-    key:"logined",
-    default:false
-})
+  key: "logined",
+  default: false,
+});
 
 export const baseurl = atom({
-    key:"baseurl",
-    default:"."
-})
+  key: "baseurl",
+  default: ".",
+});
 
 export const navatom = atom({
-    key:"navatom",
-    default:3
-})
+  key: "navatom",
+  default: 3,
+});
 
 export const userinfo = atom({
-    key:"userinfo",
-    default:{}
-})
+  key: "userinfo",
+  default: {},
+});
 
 export const payment = atom({
-    key:"payment",
-    default:{}
-})
+  key: "payment",
+  default: {},
+});
 
 export const request = atom({
-    key:"request",
-    default:[]
-})
+  key: "request",
+  default: [],
+});
