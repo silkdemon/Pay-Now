3c60b283e856fc80f5f3f7147d50dc821caf45cd
olgabukhareva <buola@yandex.ru>
2024-10-22 18:02:46 +0200

    add page object

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,8 +1,9 @@
 import { expect, test } from '@playwright/test'
 
 test('open main page', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
+  await page.goto('/')
 
+  await expect(page).toHaveTitle('Pay Now')
   await expect(
     page.getByText('Create Your Account With Zero Charges'),
   ).toBeVisible()
--- tests/registration.spec.ts
+++ tests/registration.spec.ts
@@ -13,6 +13,7 @@
 
 import { test, expect } from '@playwright/test'
 import { faker} from '@faker-js/faker'
+import { PageManager } from '../page-objects/pageManager'
 
 const testEmail = faker.internet.email({firstName: 'test', provider:'test.com'})
 const testPasword = faker.internet.password({length: 6})
@@ -21,19 +22,14 @@ const testPhoneNumber = faker.string.numeric(10)
 const testWalletPin = faker.internet.password({length: 6, pattern:/[0-9]/})
 
 test('registration', async ({page}) => {
+    const pm = new PageManager(page)
+
     await page.goto('/')
-    await page.getByText('Create account').click()
+    await pm.onInitialPage.clickCreateAccount()
 
     await expect(page).toHaveURL(/.*register/)
 
-    await page.getByLabel('Your email').fill(testEmail)
-    await page.getByLabel('Password', {exact: true}).fill(testPasword)
-    await page.getByLabel('Confirm password').fill(testPasword)
-    await page.getByPlaceholder('Your Name').fill(testName)
-    await page.getByLabel('Phone Number').fill(testPhoneNumber)
-    await page.getByLabel('Create Wallet pin').fill(testWalletPin)
-    await page.getByLabel('I accept theTerms and').check()
-    await page.getByRole('button', { name: 'Create an account' }).click()
+    await pm.onRegistrationPage.fillInRegistrationFormAndClickCreateButton(testEmail, testPasword, testName, testPhoneNumber, testWalletPin)
     const registrationResponse = await page.waitForResponse('http://localhost:3000/auth/register')
     expect(registrationResponse.ok()).toBeTruthy()
     await expect(page).toHaveURL(/.*home/)
