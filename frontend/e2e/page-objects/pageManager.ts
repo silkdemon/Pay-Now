c72769ba6a9634d44b4b3b617e19cb51df62ed33
Maria Shevchuk <ShevchukMV@cloudx.group>
2024-10-25 13:44:46 +0300

    fix

---
+++ frontend/e2e/page-objects/pageManager.ts
@@ -0,0 +1,24 @@
+import { Page } from '@playwright/test'
+import { InitialPage } from './initialPage'
+import { RegistrationPage } from './registrationPage'
+
+export class PageManager{
+
+    private readonly page: Page
+    private readonly initialPage: InitialPage
+    private readonly registrationPage: RegistrationPage
+
+    constructor(page: Page){
+        this.page = page
+        this.initialPage = new InitialPage(this.page)
+        this.registrationPage = new RegistrationPage(this.page)
+    }
+
+    get onInitialPage(){
+        return this.initialPage
+    }
+    
+    get onRegistrationPage(){
+        return this.registrationPage
+    }
+}
---
+++ frontend/e2e/page-objects/registrationPage.ts
@@ -0,0 +1,28 @@
+import { Page } from '@playwright/test';
+
+export class RegistrationPage {
+    readonly page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    /**
+     * Fill in registration form with passed data and click Create account button. All parameters are optional
+     * @param email
+     * @param password - valid value is min 6 characters
+     * @param name
+     * @param phone - valid value is string of 10 digits
+     * @param pin - valid valuue is string of 6 digits
+     */
+    async fillInRegistrationFormAndClickCreateButton(email?: string, password?: string, name?: string, phone?: string, pin?: string) {
+        if (email) await this.page.getByLabel('Your email').fill(email);
+        if (password) await this.page.getByLabel('Password', { exact: true }).fill(password);
+        if (password) await this.page.getByLabel('Confirm password').fill(password);
+        if (name) await this.page.getByPlaceholder('Your Name').fill(name);
+        if (phone) await this.page.getByLabel('Phone Number').fill(phone);
+        if (pin) await this.page.getByLabel('Create Wallet pin').fill(pin);
+        await this.page.getByLabel('I accept theTerms and').check();
+        await this.page.getByRole('button', { name: 'Create an account' }).click();
+    }
+}
