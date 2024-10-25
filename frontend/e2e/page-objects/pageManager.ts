ecf4dea32d8b54dde7bb5d704fb3067473d190b8
Maria Shevchuk <ShevchukMV@cloudx.group>
2024-10-25 17:35:58 +0300

    refactor

--- frontend/e2e/page-objects/pageManager.ts
+++ frontend/e2e/page-objects/pageManager.ts
@@ -1,24 +1,23 @@
-import { Page } from '@playwright/test'
-import { InitialPage } from './initialPage'
-import { RegistrationPage } from './registrationPage'
+import { Page } from '@playwright/test';
+import { InitialPage } from './initialPage';
+import { RegistrationPage } from './registrationPage';
 
-export class PageManager{
+export class PageManager {
+    private readonly page: Page;
+    private readonly initialPage: InitialPage;
+    private readonly registrationPage: RegistrationPage;
 
-    private readonly page: Page
-    private readonly initialPage: InitialPage
-    private readonly registrationPage: RegistrationPage
-
-    constructor(page: Page){
-        this.page = page
-        this.initialPage = new InitialPage(this.page)
-        this.registrationPage = new RegistrationPage(this.page)
+    constructor(page: Page) {
+        this.page = page;
+        this.initialPage = new InitialPage(this.page);
+        this.registrationPage = new RegistrationPage(this.page);
     }
 
-    get onInitialPage(){
-        return this.initialPage
+    get onInitialPage() {
+        return this.initialPage;
     }
-    
-    get onRegistrationPage(){
-        return this.registrationPage
+
+    get onRegistrationPage() {
+        return this.registrationPage;
     }
 }
