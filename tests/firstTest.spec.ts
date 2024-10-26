7d08788891d3900e944479abe4ffdcb308f58bdc
Maria Shevchuk <ShevchukMV@cloudx.group>
2024-10-26 12:10:27 +0300

    refactor

--- tests/firstTest.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('open main page', async ({ page }) => {
-    await page.goto('/');
-
-    await expect(page).toHaveTitle('Pay Now');
-    await expect(page.getByText('Create Your Account With Zero Charges')).toBeVisible();
-});
